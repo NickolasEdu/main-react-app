f8994935f8d787903bed64cb4487365547ded086
Nickolas Eduardo <nickolasvieira.el@gmail.com>
2022-02-10 20:35:52 -0300

    Starting to change the css files for styles components

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -1,9 +1,17 @@
-import "../styles/Header.css"
+import styled from 'styled-components'
+
+const Navbar = styled.header`
+    background-color: rgb(21, 21, 133);
+    color: #f5f5f5;
+    text-align: center;
+    height: 100%;
+    width: 100%;
+`
 
 export default function Header() {
     return(
-        <div className="Navbar">
+        <Navbar>
             <h1>This is the navbar</h1>
-        </div>
+        </Navbar>
     )
 }
--- src/partials/Profilehold.js
+++ src/partials/Profilehold.js
@@ -1,12 +1,25 @@
-import '../styles/Profile.css'
+import styled from 'styled-components'
+
+const Main = styled.div`
+    margin: 25px auto;
+    width: 25%;
+    padding: 20px;
+    border-radius: 1%;
+    box-shadow: 0 0 10px rgb(21, 21, 133);
+`
+
+const Photo = styled.img`
+    width: 100%;
+    height: 80%;
+`
 
 const Profilehold = ({ image, name, bio}) => {
     return(
-        <div className="Profile">
-            <img src={image} />
+        <Main>
+            <Photo src={image} />
             <h1>{name}</h1>
             <p>{bio}</p>
-        </div>
+        </Main>
         
     )
 }
