95a6b88ee2ec7d730f54265687f32cbd51b25e33
Nickolas Eduardo <nickolasvieira.el@gmail.com>
2022-02-12 09:42:06 -0300

    Routes defined : render bug

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
-import Profilehold from "./partials/Profilehold"
-import Header from "./partials/Header"
-import Box from "./partials/Box"
-import Boxes from "./partials/Boxes"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-import imgCoding from "./img/codinghold.jpg"
-import imgDev from "./img/dev.jpg"
-import imgFront from "./img/front.jpg"
-import imgMain from "./img/imgMain.jpg"
+import Content from './pages/Content'
+import Counter from "./components/Counter"
 
 function App() {
   return (
     <>
-        <Header />
-
-        <Profilehold
-        image={imgMain}
-        name="Nickolas Eduardo"
-        bio="Description of place holder"
-        />
-
-        <Boxes>
-          <Box
-          image={imgCoding}
-          title="Projeto 1"
-          text="aqui vai o projeto 3"
-          />
-
-          <Box 
-          image={imgDev}
-          title="Projeto 2"
-          text="Aqui vai o projeto 2"
-          />
-
-          <Box
-          image={imgFront}
-          title="Projeto 3"
-          text="Aqui vai o projeto 3"
-          />
-        </Boxes>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Content />} />
+            <Route path='/Counter' element={<Counter />} />
+          </Routes>
+        </Router>
     </>
   )
 }
--- src/pages/Content.js
+++ src/pages/Content.js
@@ -1,8 +1,43 @@
+import Profilehold from "../partials/Profilehold"
+import Header from "../partials/Header"
+import Box from "../partials/Box"
+import Boxes from "../partials/Boxes"
 
+import imgCoding from "../img/codinghold.jpg"
+import imgDev from "../img/dev.jpg"
+import imgFront from "../img/front.jpg"
+import imgMain from "../img/imgMain.jpg"
 
 function Content() {
-    return(
+    return (
         <>
+            <Header />
+
+            <Profilehold
+                image={imgMain}
+                name="Nickolas Eduardo"
+                bio="Description of place holder"
+            />
+
+            <Boxes>
+                <Box
+                    image={imgCoding}
+                    title="Projeto 1"
+                    text="aqui vai o projeto 3"
+                />
+
+                <Box
+                    image={imgDev}
+                    title="Projeto 2"
+                    text="Aqui vai o projeto 2"
+                />
+
+                <Box
+                    image={imgFront}
+                    title="Projeto 3"
+                    text="Aqui vai o projeto 3"
+                />
+            </Boxes>
         </>
     )
 }
